b0d3fa09c9d7738c614539e7a75801f283473659
HAKI <96266332+harleiaki@users.noreply.github.com>
2023-08-28 19:40:06 -0300

    Update app_7.py

--- app_7.py
+++ app_7.py
@@ -21,7 +21,7 @@ def load_data(file_data):
         return pd.read_excel(file_data)
 
 # Função para filtrar baseado na multiseleção de categorias
-@st.cache_resource(allow_output_mutation=True)
+@st.cache(allow_output_mutation=True)
 def multiselect_filter(relatorio, col, selecionados):
     if '' in selecionados:
         return relatorio
@@ -29,12 +29,12 @@ def multiselect_filter(relatorio, col, selecionados):
         return relatorio[relatorio[col].isin(selecionados)].reset_index(drop=True)
 
 # Função para converter o df para csv
-@st.cache_data
+@st.cache_resource
 def convert_df(df):
     return df.to_csv(index=False).encode('utf-8')
 
 # Função para converter o df para excel
-@st.cache_data
+@st.cache_resource
 def to_excel(df):
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')

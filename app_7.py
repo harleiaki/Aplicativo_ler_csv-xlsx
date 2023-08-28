d5284f6defeeb1f8b004509779b51a431cbda3db
HAKI <96266332+harleiaki@users.noreply.github.com>
2023-08-28 18:34:28 -0300

    Update app_7.py

--- app_7.py
+++ app_7.py
@@ -39,7 +39,7 @@ def to_excel(df):
     output = BytesIO()
     writer = pd.ExcelWriter(output, engine='xlsxwriter')
     df.to_excel(writer, index=False, sheet_name='Sheet1')
-    writer.close()
+    writer.save()
     processed_data = output.getvalue()
     return processed_data
 
@@ -195,7 +195,7 @@ def main():
         # PLOTS    
         if graph_type == 'Barras':
             sns.barplot(x = bank_raw_target_perc.index, 
-                        y = y,
+                        y = 'y',
                         data = bank_raw_target_perc, 
                         ax = ax[0])
             ax[0].bar_label(ax[0].containers[0])
@@ -203,7 +203,7 @@ def main():
                             fontweight ="bold")
             
             sns.barplot(x = bank_target_perc.index, 
-                        y = y, 
+                        y = 'y', 
                         data = bank_target_perc, 
                         ax = ax[1])
             ax[1].bar_label(ax[1].containers[0])
